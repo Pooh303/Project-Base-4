eed870349c5a5556ccbe87f9427300ae638b51f3
KinNyKry <65070184@kmitl.ac.th>
2022-11-08 19:55:53 +0700

    JUMP and BORDER
    i think i will make double jump

--- fighter.py
+++ fighter.py
@@ -3,10 +3,14 @@ import pygame
 class Agent():
     def __init__(self, x, y):
         self.rect = pygame.Rect((x, y, 80, 180))
+        self.vel_y = 0
+        self.jump = False
+        self.jump_count = 0
 
-    def move(self):
+    def move(self, screen_width, screen_height):
         """move left right"""
         SPEED = 10
+        GRAVITY = 0.5
         dx = 0
         dy = 0
         
@@ -17,7 +21,28 @@ class Agent():
             dx = -SPEED
         if key[pygame.K_d]:
             dx = SPEED
-            
+        #jump
+        if key[pygame.K_w] and self.jump == False:
+            self.vel_y = -15
+            self.jump = True
+        
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+        
+        
+        
+        # ไม่ให้เดินออกจอ
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > screen_width:
+            dx = screen_width - self.rect.right
+        if self.rect.bottom + dy > screen_height - 200:
+            self.vel_y = 0
+            self.jump = False
+            self.jump_count = 0
+            dy = screen_height - 200 - self.rect.bottom
+        
+        
         self.rect.x += dx
         self.rect.y += dy
     
--- play.py
+++ play.py
@@ -1,5 +1,6 @@
 import pygame
 from fighter import Agent #import Agent from fighter
+# import menu
 
 pygame.init()
 
@@ -8,6 +9,10 @@ SCREEN_HEIGHT = 720
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Project")
+
+
+clock = pygame.time.Clock()
+FPS = 144 #fps
 
 bg_image = pygame.image.load("assets/images/background/bg.jpg").convert_alpha() #background
 
@@ -24,12 +29,15 @@ agent_2 = Agent(800, 300) #pos spawn agent2
 
 run = True
 while run:
-    key = pygame.key.get_pressed()
+    
+    clock.tick(FPS)
+    
+    # key = pygame.key.get_pressed()
     draw_bg()
     
     #move agent
-    agent_1.move()
-    agent_2.move()
+    agent_1.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    # agent_2.move(SCREEN_WIDTH, SCREEN_HEIGHT)
     
     agent_1.draw(screen)
     agent_2.draw(screen)
@@ -37,9 +45,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    if key[pygame.K_ESCAPE]:
-            import menu
-            #กดescแล้วเข้าหน้า menu 
+    # if key[pygame.K_ESCAPE]:
+    #     menu()
+             #กดescแล้วเข้าหน้า menu 
 
 
     pygame.display.update() #update display
